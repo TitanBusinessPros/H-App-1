c40cca7d6e1d15a2f4679ec7b767d3c3396043d0
Titan Buisness Pros <titanbusinesspros@gmail.com>
2026-09-24 17:14:57 -0500

    Add main menu navigation to all tools

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'titan-halloween-happ1-';
-const CACHE_NAME = `${CACHE_PREFIX}v15`;
+const CACHE_NAME = `${CACHE_PREFIX}v16`;
 const LEGACY_CACHE_NAME = 'titan-halloween-tools-v14';
 const APP_SHELL = [
   './',
